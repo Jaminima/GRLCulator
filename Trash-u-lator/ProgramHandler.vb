8fe17a9fd934bf79310aed20b0dd4672cca40f28
Oscar Davies <oscar.davies@hotmail.co.uk>
2018-02-09 16:05:07 +0000

    Direct addressing

--- Trash-u-lator/ProgramHandler.vb
+++ Trash-u-lator/ProgramHandler.vb
@@ -7,13 +7,13 @@
             Operand2 As String = Form1.MemoryGrid.Item("Operand2", CInt(Form1.Txt_PC.Text) - 1).Value,
             Operand3 As String = Form1.MemoryGrid.Item("Operand3", CInt(Form1.Txt_PC.Text) - 1).Value
 
-            If Opcode = "LDR" Then LDR(Operand1, Operand2)
-            If Opcode = "STR" Then STR(Operand1, Operand2)
-            If Opcode = "MOV" Then MOV(Operand1, Operand2)
-            If Opcode = "ADD" Then Math_ADD(Operand1, Operand2, Operand3)
-            If Opcode = "SUB" Then Math_SUB(Operand1, Operand2, Operand3)
-            If Opcode = "JMP" Then JMP(Operand1)
-            If Opcode = "HALT" Then HALT()
+            If Opcode = "LDR" Then OpCodeHandlerNew.LDR(Operand1, Operand2)
+            If Opcode = "STR" Then OpCodeHandlerNew.STR(Operand1, Operand2)
+            If Opcode = "MOV" Then OpCodeHandlerNew.MOV(Operand1, Operand2)
+            If Opcode = "ADD" Then OpCodeHandlerNew.Math_ADD(Operand1, Operand2, Operand3)
+            If Opcode = "SUB" Then OpCodeHandlerNew.Math_SUB(Operand1, Operand2, Operand3)
+            If Opcode = "JMP" Then OpCodeHandlerNew.JMP(Operand1)
+            If Opcode = "HALT" Then OpCodeHandlerNew.HALT()
         Catch
             Form1.Tmr_Auto.Stop()
             Form1.Btn_Auto.BackColor = Color.Red
@@ -26,26 +26,26 @@
 
 End Module
 
-Module OpCodeHandler
+Module OpCodeHandlerNew
 
     Sub LDR(Operand1, Operand2)
-        SharedCode._GetControl(Operand1).Text = Form1.MemoryGrid.Item("Operand1", CInt(Operand2) - 1).Value
+        SharedCode._GetControl(Operand1).Text = AddressForm(Operand2)
     End Sub
 
     Sub STR(Operand1, Operand2)
-        Form1.MemoryGrid.Item("Operand1", CInt(Operand2) - 1).Value = SharedCode._GetControl(Operand1).Text
+        Form1.MemoryGrid.Item("Operand1", CInt(Operand2)).Value = AddressForm(Operand1)
     End Sub
 
     Sub MOV(Operand1, Operand2)
-        SharedCode._GetControl(Operand1).Text = Operand2
+        LDR(Operand1, Operand2)
     End Sub
 
     Sub Math_ADD(Operand1, Operand2, Operand3)
-        SharedCode._GetControl(Operand1).Text = CInt(SharedCode._GetControl(Operand2).Text) + CInt(Form1.MemoryGrid("Operand1", CInt(Operand3) - 1).Value)
+        SharedCode._GetControl(Operand1).Text = CInt(AddressForm(Operand2)) + CInt(AddressForm(Operand3))
     End Sub
 
     Sub Math_SUB(Operand1, Operand2, Operand3)
-        SharedCode._GetControl(Operand1).Text = CInt(SharedCode._GetControl(Operand2).Text) - CInt(Form1.MemoryGrid("Operand1", CInt(Operand3) - 1).Value)
+        SharedCode._GetControl(Operand1).Text = CInt(AddressForm(Operand2)) - CInt(AddressForm(Operand3))
     End Sub
 
     Sub JMP(Operand1)
@@ -57,5 +57,42 @@ Module OpCodeHandler
         Form1.Btn_Auto.BackColor = Color.Red
         Form1.Run = False
     End Sub
+
+End Module
+
+
+
+
+Module OpCodeHandler
+
+    '    Sub LDR(Operand1, Operand2)
+    '        SharedCode._GetControl(Operand1).Text = Form1.MemoryGrid.Item("Operand1", CInt(Operand2) - 1).Value
+    '    End Sub
+
+    '    Sub STR(Operand1, Operand2)
+    '        Form1.MemoryGrid.Item("Operand1", CInt(Operand2) - 1).Value = SharedCode._GetControl(Operand1).Text
+    '    End Sub
+
+    '    Sub MOV(Operand1, Operand2)
+    '        SharedCode._GetControl(Operand1).Text = Operand2
+    '    End Sub
+
+    '    Sub Math_ADD(Operand1, Operand2, Operand3)
+    '        SharedCode._GetControl(Operand1).Text = CInt(SharedCode._GetControl(Operand2).Text) + CInt(Form1.MemoryGrid("Operand1", CInt(Operand3) - 1).Value)
+    '    End Sub
+
+    '    Sub Math_SUB(Operand1, Operand2, Operand3)
+    '        SharedCode._GetControl(Operand1).Text = CInt(SharedCode._GetControl(Operand2).Text) - CInt(Form1.MemoryGrid("Operand1", CInt(Operand3) - 1).Value)
+    '    End Sub
+
+    '    Sub JMP(Operand1)
+    '        Form1.Txt_PC.Text = CInt(Operand1) - 1
+    '    End Sub
+
+    '    Sub HALT()
+    '        Form1.Tmr_Auto.Stop()
+    '        Form1.Btn_Auto.BackColor = Color.Red
+    '        Form1.Run = False
+    '    End Sub
 
 End Module
--- Trash-u-lator/SharedCode.vb
+++ Trash-u-lator/SharedCode.vb
@@ -15,4 +15,15 @@
         Next
     End Function
 
+    Function AddressForm(Address As String) As String
+        If Address.Contains("#") Then
+            Address = Address.TrimStart("#")
+            Return Address
+        ElseIf Address.ToCharArray()(0) = "R" Then
+            Return _GetControl(Address).Text
+        Else
+            Return Form1.MemoryGrid("Operand1", CInt(Address) - 1).Value
+        End If
+    End Function
+
 End Module
